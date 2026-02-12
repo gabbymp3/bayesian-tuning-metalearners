8f5dd31203a5d46ddc187770bc7245ba3db55d68
gabbymp3 <159870927+gabbymp3@users.noreply.github.com>
2026-02-11 18:01:35 -0600

    add init experiment configs

---
+++ src/experiment_configs/config_1d.py
@@ -0,0 +1,83 @@
+from sklearn.ensemble import RandomForestRegressor
+from catboost import CatBoostRegressor
+from sklearn.linear_model import LogisticRegression
+from skopt.space import Integer, Real
+from src.tuning import grid_search, random_search, bayesian_search
+
+
+R = 5
+dgp_params = {"N": 1000, "d": 15, "alpha": 0.5}
+
+
+# -------------------------
+# RANDOM FOREST CONFIG
+# -------------------------
+rf_config = {
+    "name": "x_rf",
+    "models": RandomForestRegressor(random_state=0),
+    "propensity_model": LogisticRegression(),
+}
+
+rf_tuners = [
+    {
+        "name": "grid",
+        "fn": grid_search,
+        "param_grid": {
+            "models__n_estimators": [20, 50, 80, 110, 140]
+        },
+        "kwargs": {"cv": 3}
+    },
+    {
+        "name": "random",
+        "fn": random_search,
+        "param_dist": {
+            "models__n_estimators": Integer(20, 150)
+        },
+        "kwargs": {"cv": 3, "n_iter": 10}
+    },
+    {
+        "name": "bayes",
+        "fn": bayesian_search,
+        "param_dist": {
+            "models__n_estimators": Integer(20, 150)
+        },
+        "kwargs": {"cv": 3, "n_iter": 10}
+    },
+]
+
+
+# -------------------------
+# CATBOOST CONFIG
+# -------------------------
+cb_config = {
+    "name": "x_cb",
+    "models": CatBoostRegressor(verbose=0, random_state=0),
+    "propensity_model": LogisticRegression(),
+}
+
+cb_tuners = [
+    {
+        "name": "grid",
+        "fn": grid_search,
+        "param_grid": {
+            "models__learning_rate": [0.01, 0.03, 0.05, 0.1, 0.15]
+        },
+        "kwargs": {"cv": 3}
+    },
+    {
+        "name": "random",
+        "fn": random_search,
+        "param_dist": {
+            "models__learning_rate": Real(0.01, 0.15)
+        },
+        "kwargs": {"cv": 3, "n_iter": 10}
+    },
+    {
+        "name": "bayes",
+        "fn": bayesian_search,
+        "param_dist": {
+            "models__learning_rate": Real(0.01, 0.15)
+        },
+        "kwargs": {"cv": 3, "n_iter": 10}
+    },
+]
---
+++ src/experiment_configs/config_6d.py
@@ -0,0 +1,87 @@
+from sklearn.ensemble import RandomForestRegressor
+from catboost import CatBoostRegressor
+from sklearn.linear_model import LogisticRegression
+from skopt.space import Integer, Real
+from src.tuning import random_search, bayesian_search
+
+
+R = 5
+dgp_params = {"N": 1000, "d": 15, "alpha": 0.5}
+
+
+# -------------------------
+# RANDOM FOREST CONFIG
+# -------------------------
+rf_config = {
+    "name": "x_rf",
+    "models": RandomForestRegressor(random_state=0),
+    "propensity_model": LogisticRegression(),
+}
+
+rf_tuners = [
+    {
+        "name": "random",
+        "fn": random_search,
+        "param_dist": {
+            "models__n_estimators": Integer(20, 150),
+            "models__max_depth": Integer(3, 10),
+            "models__min_samples_leaf": Integer(1, 20),
+            "models__min_samples_split": Integer(2, 20),
+            "models__max_features": Real(0.1, 1.0),
+            "models__max_leaf_nodes": Integer(2, 50)
+        },
+        "kwargs": {"cv": 3, "n_iter": 60}
+    },
+    {
+        "name": "bayes",
+        "fn": bayesian_search,
+        "param_dist": {
+            "models__n_estimators": Integer(20, 150),
+            "models__max_depth": Integer(3, 10),
+            "models__min_samples_leaf": Integer(1, 20),
+            "models__min_samples_split": Integer(2, 20),
+            "models__max_features": Real(0.1, 1.0),
+            "models__max_leaf_nodes": Integer(2, 50)
+        },
+        "kwargs": {"cv": 3, "n_iter": 60}
+    },
+]
+
+
+# -------------------------
+# CATBOOST CONFIG
+# -------------------------
+cb_config = {
+    "name": "x_cb",
+    "models": CatBoostRegressor(verbose=0, random_state=0),
+    "propensity_model": LogisticRegression(),
+}
+
+cb_tuners = [
+    {
+        "name": "random",
+        "fn": random_search,
+        "param_dist": {
+            "models__learning_rate": Real(0.01, 0.15),
+            "models__depth": Integer(3, 10),
+            "models__iterations": Integer(50, 300),
+            "models__l2_leaf_reg": Integer(1, 10),
+            "models__random_strength": Real(0.01, 10),
+            "models__subsample": Real(0.1, 1.0)
+        },
+        "kwargs": {"cv": 3, "n_iter": 60}
+    },
+    {
+        "name": "bayes",
+        "fn": bayesian_search,
+        "param_dist": {
+            "models__learning_rate": Real(0.01, 0.15),
+            "models__depth": Integer(3, 10),
+            "models__iterations": Integer(50, 300),
+            "models__l2_leaf_reg": Integer(1, 10),
+            "models__random_strength": Real(0.01, 10),
+            "models__subsample": Real(0.1, 1.0)
+        },
+        "kwargs": {"cv": 3, "n_iter": 60}
+    },
+]
