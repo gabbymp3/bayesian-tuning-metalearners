1604109e86af54687707b7d499fa070680c1caa7
gabbymp3 <159870927+gabbymp3@users.noreply.github.com>
2026-03-10 22:01:45 -0500

    organize repo

--- src/main.py
+++ src/main.py
@@ -14,10 +14,10 @@ warnings.filterwarnings("ignore")
 # -------------------------------------------------
 
 CONFIGS_TO_RUN = [
-    #"config_1d",
-    #"config_2d",
+    "config_1d",
+    "config_2d",
     "config_4d",
-    #"config_6d",
+    "config_6d",
 ]
 
 
@@ -37,6 +37,8 @@ for config_name in CONFIGS_TO_RUN:
     config = module.get_config()
 
     for learner in config["learners"]:
+        #if learner["name"] == "x_rf":
+            #continue
 
         print(f"  → Learner: {learner['name']}")
         print("-"*80)
