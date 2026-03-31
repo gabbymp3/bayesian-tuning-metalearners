9f5ff8ff6b020fa355346e1bb4a20d8574e9efff
Gabriella <gabbyvjm@gmail.com>
2026-03-31 12:10:36 -0500

    update logs + bash script

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=bayes_tuning
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
-#SBATCH --time=46:00:00
+#SBATCH --time=72:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --partition=normal
+#SBATCH --partition=long
 
 module purge
 module load python/3.13.0-gcc-8.5.0
